0664586d5096bc3595f6b97051d14d5a7e35604e
Greg Albrecht <gba@onbeep.com>
2016-10-18 23:57:19 -0700

    minor bump

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ Source:: https://github.com/ampledata/weewx_graphite
 
 
 __title__ = 'graphite'
-__version__ = '2.0.0'
+__version__ = '2.1.0'
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'
 __license__ = 'Apache License, Version 2.0'
 __copyright__ = 'Copyright 2016 Orion Labs, Inc.'
